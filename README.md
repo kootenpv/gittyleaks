c347626900755be76749c6922e75c282b44f5a31
Pascal van Kooten <pvkooten@jibes.nl>
2015-08-02 00:58:36 +0200

    changed to the functioning and also readme

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 <table>
 <tr>
 <td>
-<img src="https://github.com/kootenpv/gittyleaks/raw/master/resources/gittyleaks.png" height="400px" width="400px"/>
+<img src="https://github.com/kootenpv/gittyleaks/raw/master/resources/gittyleaks.png" height="400px" width="600px"/>
 </td>
 <td>
 Very often it happens that when mocking/just starting out with a new project on github, sensitive data gets added. API keys, usernames, passwords and emails are easily added.... and then forgotten. Use this tool to detect where the mistakes are in your repos.
@@ -13,11 +13,14 @@ Very often it happens that when mocking/just starting out with a new project on 
 </tr>
 </table>
 
-Run like:
+Usage examples:
 
 ```python
 # gittyleaks -u username -r repo
 gittyleaks -u kootenpv -r yagmail
+
+# gittyleaks -l some-git-cloneable
+gittyleaks https://github.com/kootenpv/yagmail
 ```
 
 another example:
@@ -26,7 +29,7 @@ another example:
 gittyleaks -u smartczy -r weather_py
 ```
 
-It is now already possible to subclass and overwrite certain methods. More docs on that to follow.
+By default `gittyleaks` tries to be smart about might be compromising. If you really want to find anything, you can use the '--find-anything' option to find things less likely to be hardcoded.
 
 #### Installation
 
@@ -37,14 +40,17 @@ pip install gittyleaks    # for python 2
 pip3 install gittyleaks   # for python 3
 ```
 
-upgrades can be done by giving the -U flag; `pip3 install -U`.
+Upgrades can be done by giving the -U flag; `pip3 install -U gittyleaks`.
 
 #### Roadmap
 
+It is now already possible to subclass and overwrite certain methods. More docs on that to follow.
+
+~~Deleting a cloned repo should also be added with perhaps adding the `-d` flag~~
+
+~~Allow user to --find-anything rather than filtering~~
+
 The package will soon provide the best practice to remedy this issue based on [https://help.github.com/articles/remove-sensitive-data/](https://help.github.com/articles/remove-sensitive-data/).
 
-Furthermore, positional arguments should also be possible.
-
-Deleting a cloned repo should also be added with perhaps adding the `-d` flag
-
 After that, quality improvements to the detection will be done.
+
--- gittyleaks/gittyleaks.py
+++ gittyleaks/gittyleaks.py
@@ -4,8 +4,13 @@
 
 import re
 from sh import git
+from sh import rm
 import sh
 import os
+import argparse
+
+def get_immediate_subdirectories():
+    return set([fname for fname in os.listdir('.') if os.path.isdir(fname)])
 
 class GittyLeak():
     def __init__(self, kwargs): 
@@ -16,57 +21,56 @@ class GittyLeak():
         assignment = "(\\b|[ ._-])({})[ '\"]*(=|:)[ '\"]*([^'\" ]+)"
         self.assignment_pattern = assignment.format('|'.join(self.keywords))
 
-        excluded_value_chars = ['.', '[', 'none', 'true', 'false', 'null', 'default', 'example']    
+        excluded_value_chars = ['.', '[', 'none', 'true', 'false', 'null', 'default', 'example', 
+                                'username', 'email', 'password']    
+
+        self.min_value_length = 4
+        
         self.excluded_value_chars = [x.lower() for x in excluded_value_chars]
 
+        self.excluding = None
         self.revision_list = []
         self.user = None
         self.repo = None
         self.link = None
-        self.strict = None 
+        self.find_anything = None 
         self.case_insensitive = True
         self.show_revision_names = False
         self.print_banner = True
-        self.do_not_delete = None
+        self.delete = None
         
         for k,v in kwargs.items():
             setattr(self, k, v)
+
+        if self.find_anything:     
+            self.excluded_value_chars = []
+            self.case_insensitive = False
+            
+        if self.excluding is not None:
+            self.excluded_value_chars.extend(self.excluding)
+        
         
     def clone(self): 
-        if self.repo is None and (self.user or self.link):
-            raise ValueError('Provide username or link')
-
         if self.link is not None:
             try:
-                git('clone', self.link)
+                pre = get_immediate_subdirectories()
+                git('clone', self.link) 
+                post = get_immediate_subdirectories()
+                self.repo = (post - pre).pop()
             except sh.ErrorReturnCode_128:
                 pass 
 
         elif self.user is not None: 
             try:
                 git('clone', 'https://github.com/{}/{}.git'.format(self.user, self.repo))
-            except sh.ErrorReturnCode_128:
-                pass 
 
-        if self.repo is not None:    
-            os.chdir(self.repo) 
+            except sh.ErrorReturnCode_128:
+                pass
+
+        os.chdir(self.repo) 
 
     def get_revision_list(self):
-        self.revision_list = git('rev-list', '--all').strip().split('\n')
-            
-    def run(self):
-        self.clone()
-
-        self.get_revision_list()
-
-        if self.strict:     
-            self.excluded_value_chars = []
-            self.case_insensitive = False
-            
-        matched_items = self.get_matches_dict() 
-        
-        self.print_matches(matched_items)
-    
+        self.revision_list = git('rev-list', '--all').strip().split('\n')                
 
     def get_git_matches(self, revision):
         try: 
@@ -83,7 +87,9 @@ class GittyLeak():
         return word_matches        
 
     def validated_value(self, v):
-        if v.strip(): 
+        if v.strip():
+            if len(v) < self.min_value_length:
+                return False
             if self.excluded_value_chars:
                 if self.case_insensitive:
                     v = v.lower()
@@ -134,14 +140,35 @@ class GittyLeak():
                 print('num_of_revisions: {}'.format(len(set([x[1] for x in v]))))        
             print('----------------------------------------')
 
-def main():
-    """ This is the function that is run from commandline with `gittyleaks` """ 
-    import argparse
+    def run(self):
+        if (self.user and self.repo) or self.link:
+            self.clone()
+
+        self.get_revision_list()
+
+        matched_items = self.get_matches_dict() 
+        
+        self.print_matches(matched_items)
+
+        if self.delete and (self.user and self.repo) or self.link:
+            rm('-rf', '../' + self.repo)
+
+            
+def get_args_parser():
     parser = argparse.ArgumentParser(description='Discover where your sensitive data has been leaked.') 
     parser.add_argument('-user', '-u', help='Provide the user/owner of the repo, only if cloning from github') 
     parser.add_argument('-repo', '-r', help='Provide the name of the repo, only if cloning from github') 
-    parser.add_argument('-link', '-l', 
-                        help='Default is to load from github, provide the full url (link) to clone that instead') 
-    args = parser.parse_args() 
+    parser.add_argument('-link', '-l', help='Default is to load from github, provide the full url (link) to clone that instead') 
+    parser.add_argument('-delete', '-d', action='store_true',
+                        help = 'If cloned, remove the repo afterwards')
+    parser.add_argument('--find-anything', '-a', action='store_true',
+                        help='If you really want to find anything remotely suspicious, set this flag')
+    parser.add_argument('--excluding', '-e', nargs = '+',
+                        help='List of words that are ignored when they occur as value. E.g. you might want to exclude $ signs whenever they occur in the value')
+    return parser
+            
+def main():
+    """ This is the function that is run from commandline with `gittyleaks` """ 
+    args = get_args_parser().parse_args() 
     gl = GittyLeak(args.__dict__)    
     gl.run() 
